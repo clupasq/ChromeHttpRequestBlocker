85c61868cae2b79ae7f3507388ce24e50334f2de
Cristian Lupascu <cristi.lupascu@gmail.com>
2017-07-01 14:39:06 +0300

    Formatted to 2 spaces

--- src/blocker.js
+++ src/blocker.js
@@ -1,48 +1,48 @@
 function blockRequest(details) {
-    console.log("Blocked: ", details.url);
-    return {
-        cancel: true
-    };
+  console.log("Blocked: ", details.url);
+  return {
+    cancel: true
+  };
 }
 
 function updateFilters(urls) {
-    console.info('Updating...');
+  console.info('Updating...');
 
-    if (chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
-        chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
+  if (chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
+    chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
 
-    if (patterns.length) {
-        chrome.webRequest.onBeforeRequest.addListener(blockRequest, {
-            urls: patterns
-        }, ['blocking']);
-        console.info('Using patterns:');
-        for (p in patterns) {
-            console.info(patterns[p]);
-        }
-    } else {
-        console.warn('No patterns defined.');
+  if (patterns.length) {
+    chrome.webRequest.onBeforeRequest.addListener(blockRequest, {
+      urls: patterns
+    }, ['blocking']);
+    console.info('Using patterns:');
+    for (p in patterns) {
+      console.info(patterns[p]);
     }
+  } else {
+    console.info('No patterns defined yet.');
+  }
 
-    console.info('Updated!')
+  console.info('Updated!')
 }
 
 function load(callback) {
-    chrome.storage.sync.get('blocked_patterns', function(data) {
-        callback(data['blocked_patterns'] || []);
-    });
+  chrome.storage.sync.get('blocked_patterns', function(data) {
+    callback(data['blocked_patterns'] || []);
+  });
 }
 
 function save(newPatterns, callback) {
-    patterns = newPatterns;
-    chrome.storage.sync.set({
-        'blocked_patterns': newPatterns
-    }, function() {
-        updateFilters();
-        callback.call();
-    });
+  patterns = newPatterns;
+  chrome.storage.sync.set({
+    'blocked_patterns': newPatterns
+  }, function() {
+    updateFilters();
+    callback.call();
+  });
 }
 
 load(function(p) {
-    patterns = p;
-    updateFilters();
+  patterns = p;
+  updateFilters();
 });
